75c683e17840cdcfa77872953565914caff217a6
matts <mstrocc@gmail.com>
2016-05-17 21:40:10 -0700

    class4ex1

--- class4ex1_paramiko.py
+++ class4ex1_paramiko.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import paramiko
+import time
+
 from getpass import getpass
 password = getpass(prompt="pyclass password: ")
 username = 'pyclass'
@@ -10,9 +12,17 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False, port=port)
 ssh_connect = ssh.invoke_shell()
-ssh.send('terminal length 0\n')
-ssh.send('sh version\n')
-output = ssh.recv(65535)
+output = ssh_connect.recv(65535)
+print output
+
+ssh_connect.send("terminal length 0\n")
+time.sleep(1)
+ssh_connect.send("\n")
+time.sleep(1)
+
+ssh_connect.send('sh version\n')
+time.sleep(3)
+output = ssh_connect.recv(65535)
 print output
 
 
