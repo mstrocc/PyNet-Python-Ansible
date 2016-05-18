c1d1033fb2c2353d9bc33b319040ad5c251c3b2a
MattS <mstrocc@gmail.com>
2016-05-17 21:18:20 -0700

    Built class4ex1_paramiko script

--- class4ex1_paramiko.py
+++ class4ex1_paramiko.py
@@ -2,13 +2,13 @@
 
 import paramiko
 from getpass import getpass
-password = getpass(prompt="pyclass password")
+password = getpass(prompt="pyclass password: ")
 username = 'pyclass'
 ip = '50.76.53.27'
 port = 8022
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(ip_addr, username=username, password=password, look_for_keys=False, allow_agent=False, port=port)
+ssh.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False, port=port)
 ssh_connect = ssh.invoke_shell()
 ssh.send('terminal length 0\n')
 ssh.send('sh version\n')
