c8ed99982acfb588102a9ef5ecda7c607de2b5c1
matts <mstrocc@gmail.com>
2016-05-18 21:53:35 -0700

    adding class4ex5

---
+++ class4ex5_netmiko1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from getpass import getpass
+from netmiko import ConnectHandler
+from test_devices import pynet1, pynet2, juniper_srx
+
+def main():
+    '''
+    Using Netmiko enter into configuation mode on a network device.
+
+    Verify that you are currently in configration mode.
+    '''
+
+    ip_addr = raw_input("Enter IP Address: ")
+    password = getpass()
+
+    for a_dict in (pynet1, pynet2, juniper_srx):
+        a_dict['ip'] = ip_addr
+        a_dict['password'] = password
+
+        net_connect2 = ConnectHandler(**pynet2)
+        net_connect2.config_mode()
+
+        print "\n>>>"
+        print "Checking pynet-rtr2 is in configuration mode."
+        print "Config mode check: {}".format(net_connect2.check_config_mode())
+        print "Current prompt: {}".format(net_connect2.find_prompt())
+        print ">>>>\n"
+
+if __name__ == "__main__":
+    main()
---
+++ test_devices.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# This is dictionary file containgn basic login information for devices we want to connect to.  It is then imported into the python scripts we decide to use.  It saves itmeon having to reenter the same informaiton over and over again.
+
+pynet1 = {
+    'device_type': 'cisco_ios',
+    'username': 'pyclass',
+}
+
+pynet2 = {
+    'device_type': 'cisco_ios',
+    'username': 'pyclass',
+    'secret': '',
+    'port': 8022,
+}
+
+juniper_srx = {
+    'device_type': 'juniper',
+    'username': 'pyclass',
+    'port': 9822,
+}
+    
