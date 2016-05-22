03d274a9c0d8b30d827c17e92f72b4a00c57b1b5
MattS <mstrocc@gmail.com>
2016-05-21 17:24:22 -0700

    class4ex8

---
+++ class4ex8_netmiko4.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from getpass import getpass
+from netmiko import ConnectHandler
+from test_devices import pynet1, pynet2, juniper_srx
+
+def main():
+    '''
+    Use Netmiko to change the logging buffer size on pynet-rtr2.
+    '''
+
+    ip_addr = raw_input("Enter IP address: ")
+    password = getpass()
+
+    # Get connection parameters setup correctly
+    for a_dict in (pynet1, pynet2):
+        a_dict['ip'] = ip_addr
+        a_dict['password'] = password
+        a_dict['verbose'] = False
+
+    net_connect = ConnectHandler(**a_dict)
+    net_connect.send_config_from_file(config_file=config_file.txt)
+    #config_commands = ['logging buffered 20000']
+    #net_connect.send_config_set(config_commands)
+
+    output = net_connect.send_command("show run | i logging")
+    
+    print
+    print "Device: {}:{}".format(net_connect.ip, net_connect.port)
+    print
+    print output
+    print
+
+if __name__ == "__main__":
+    main()
